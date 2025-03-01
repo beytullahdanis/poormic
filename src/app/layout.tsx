4d5f44be67e12c96d755f5b1aad0e74e4532d14f
beytullahdanis <beytullahdanis19@gmail.com>
2025-03-01 17:32:03 +0300

    upload project files

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <span className="font-bold text-2xl">poormic</span>
+    </div>
+  );
+}
